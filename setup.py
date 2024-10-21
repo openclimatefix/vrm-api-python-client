9c747c92be634356ac9ca9526005a47af3d6a90f
Matthew Duffin <mduffin95@users.noreply.github.com>
2024-10-21 22:25:57 +0100

    rename

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='vrmapi',
+    name='ocf-vrmapi',
     packages=find_packages(),
     version='0.1.1',
     description='Victron python api',
