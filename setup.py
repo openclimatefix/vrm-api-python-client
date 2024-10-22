0e5a341890510024cacf7ec0163bf28f2ac22bcb
BumpVersion Action <bumpversion@github-actions>
2024-10-22 07:59:24 +0000

    Bump version: 0.1.1 → 0.1.2 [skip ci]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='ocf_vrmapi',
     packages=find_packages(),
-    version='0.1.1',
+    version='0.1.2',
     description='Victron python api',
     install_requires=['requests'],
 )
