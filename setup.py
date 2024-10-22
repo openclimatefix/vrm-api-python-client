8b02fe260f5bee9ba5bcbdea77657183603c18ba
peterdudfield <peter.dudfield@hotmail.com>
2024-10-22 09:03:29 +0100

    add license and readme

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    readme = f.read()
+
 setup(
     name='ocf_vrmapi',
     packages=find_packages(),
     version='0.1.2',
     description='Victron python api',
+    license='MIT',
+    long_description=readme,
     install_requires=['requests'],
 )
